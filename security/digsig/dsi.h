1f4b3fcac6c50786d409ef5f4339fca71fb18062
Rupert Muchembled <rupert@rmuch.net>
2013-12-14 11:07:25 +0000

    digsig: Remove MODULE check in dsi.h
    Remove check for MODULE preprocessor definition in dsi.h, which is no longer
    required because the code will not be running outside the kernel.
    
    Signed-off-by: Rupert Muchembled <rupert@rmuch.net>

--- security/digsig/dsi.h
+++ security/digsig/dsi.h
@@ -23,31 +23,21 @@
 #ifndef _DSI_H
 #define _DSI_H
 
-#ifdef MODULE
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/pid.h>
 #include <linux/init.h>
-#endif
-
 
 /* DSM-DIGSIG error types */
 #define DSM_SUCCESS             1
 #define DSM_FAILURE             2
-
-
-#ifdef MODULE			/* This prevents user programs that include dsi.h to inherit kernel stuff */
 
 #define DIGSIG_SAFE_ALLOC (in_interrupt () ? GFP_ATOMIC : GFP_KERNEL)
 
 /* This is a hack to avoid using va_lists */
 #define DSM_ERROR(fmt,arg...) printk(DIGSIG_MODULE_NAME" Error - " fmt,##arg)
 
-
 #include "dsi_debug.h"		/* Include this file first since it contains DSM_PRINT macro */
-
-
-#endif				/* MODULE */
 
 extern int g_init;
 
